5852dc8af32be4444ec00c4649353d5399ef89a9
stefan <st.t.lang@gmx.de>
2016-06-16 11:08:50 +0200

    The RNA_analysis_script.pl script should create the initial R scripts to convert the bam files to an R object.

--- bin/RNA_analysis_script.pl
+++ bin/RNA_analysis_script.pl
@@ -27,10 +27,20 @@
        
        -options       :format: key_1 value_1 key_2 value_2 ... key_n value_n
        
-             GroupName :the column in the samples table that contains the group
-                        information - important for plotting
-                        
-
+             
+             RobjName  :The R object name - make it phony
+             
+          ## quantify reads using RSubread options; see Rsubread documentation. 
+                       
+             annot.ext :the gtf file that will be used to quantify the data   
+             
+             nthreads            default '32'
+             allowMultiOverlap   default 'TRUE'
+             GTF.attrType        default 'gene_id'
+             GTF.featureType     default 'exon'
+             isPairedEnd         default 'FALSE'
+             isGTFAnnotationFile default 'TRUE'
+		
 
        -help           :print this help
        -debug          :verbose output
@@ -62,104 +72,157 @@ my $plugin_path = "$FindBin::Bin";
 
 my $VERSION = 'v1.0';
 
-
-my ( $help, $debug, $database, @bam_files, $samples, $outfile, $options, @options);
-
-Getopt::Long::GetOptions(
-       "-bam_files=s{,}"    => \@bam_files,
-	 "-samples=s"    => \$samples,
-	 "-outfile=s"    => \$outfile,
-       "-options=s{,}"    => \@options,
-
-	 "-help"             => \$help,
-	 "-debug"            => \$debug
+my (
+	$help,    $debug,   $database, @bam_files,
+	$samples, $outfile, $options,  @options
 );
 
-my $warn = '';
+Getopt::Long::GetOptions(
+	"-bam_files=s{,}" => \@bam_files,
+	"-samples=s"      => \$samples,
+	"-outfile=s"      => \$outfile,
+	"-options=s{,}"   => \@options,
+
+	"-help"  => \$help,
+	"-debug" => \$debug
+);
+
+my $warn  = '';
 my $error = '';
 
-unless ( -f $bam_files[0]) {
+unless ( -f $bam_files[0] ) {
 	$error .= "the cmd line switch -bam_files is undefined!\n";
 }
-unless ( -f $samples) {
+unless ( -f $samples ) {
 	$error .= "the cmd line switch -samples is undefined!\n";
 }
-unless ( defined $outfile) {
+unless ( defined $outfile ) {
 	$error .= "the cmd line switch -outfile is undefined!\n";
 }
-unless ( defined $options[0]) {
+unless ( defined $options[0] ) {
 	$warn .= "the cmd line switch -options is undefined!\n";
 }
 
-
-if ( $help ){
-	print helpString( ) ;
-	exit;
-}
-
-if ( $error =~ m/\w/ ){
-	helpString($error ) ;
+if ($help) {
+	print helpString();
 	exit;
 }
 
 sub helpString {
 	my $errorMessage = shift;
-	$errorMessage = ' ' unless ( defined $errorMessage); 
+	$errorMessage = ' ' unless ( defined $errorMessage );
 	print "$errorMessage.\n";
-	pod2usage(q(-verbose) => 1);
+	pod2usage( q(-verbose) => 1 );
 }
 
+my ($task_description);
 
-my ( $task_description);
-
-$task_description .= 'perl '.$plugin_path .'/RNA_analysis_script.pl';
-$task_description .= ' -bam_files "'.join( '" "', @bam_files ).'"' if ( defined $bam_files[0]);
-$task_description .= " -samples '$samples'" if (defined $samples);
-$task_description .= " -outfile '$outfile'" if (defined $outfile);
-$task_description .= ' -options "'.join( '" "', @options ).'"' if ( defined $options[0]);
-
+$task_description .= 'perl ' . $plugin_path . '/RNA_analysis_script.pl';
+$task_description .= ' -bam_files "' . join( '" "', @bam_files ) . '"'
+  if ( defined $bam_files[0] );
+$task_description .= " -samples '$samples'" if ( defined $samples );
+$task_description .= " -outfile '$outfile'" if ( defined $outfile );
+$task_description .= ' -options "' . join( '" "', @options ) . '"'
+  if ( defined $options[0] );
 
 for ( my $i = 0 ; $i < @options ; $i += 2 ) {
 	$options[ $i + 1 ] =~ s/\n/ /g;
 	$options->{ $options[$i] } = $options[ $i + 1 ];
 }
-open ( LOG , ">$outfile.log") or die $!;
-print LOG $task_description."\n";
-close ( LOG );
+foreach ('annot.ext' ) {
+	$error .= "option $_ is missing\n" unless ( defined $options->{$_} );
+}
 
+if ( $error =~ m/\w/ ) {
+	helpString($error);
+	exit;
+}
+
+my $fm = root->filemap($outfile);
+
+###### default options ########
+$options->{'RobjName'}            ||= $fm->{'filename_core'};
+$options->{'isGTFAnnotationFile'} ||= 'TRUE';
+$options->{'GTF.featureType'}     ||= 'exon';
+$options->{'GTF.attrType'}        ||= 'gene_id';
+$options->{'allowMultiOverlap'}   ||= "TRUE";
+$options->{'isPairedEnd'}         ||= 'FALSE';
+$options->{'nthreads'}            ||= 32;
+##############################
+
+unless ( -d $fm->{'path'} ) {
+	system("mkdir -p $fm->{'path'}");
+}
+
+open( LOG, ">$outfile.log" ) or die $!;
+print LOG $task_description . "\n";
 
 ## Do whatever you want!
 
-my $data_table = data_table->new( {'filename' => $samples} );
-## now I need to find the filename column
-
-my  @fileCols = &check_4_link2files( @{$data_table->{'data'}}[0] );
-
-if (@fileCols==0 ){
-	Carp::confess ( "Sorry, but I could not link the samples table to the files you have given me!");
+## prepare the Samples.xls file
+my $data_table = data_table->new( { 'filename' => $samples } );
+my @fileCols = &check_4_link2files( @{ $data_table->{'data'} }[0] );
+if ( @fileCols == 0 ) {
+	Carp::confess(
+"Sorry, but I could not link the samples table to the files you have given me!"
+	);
 }
-if (@fileCols >1 ){
-	warn "I use the first file column @{$data_table->{'header'}}[$fileCols[0]]\n";
+if ( @fileCols > 1 ) {
+	warn
+	  "I use the first file column @{$data_table->{'header'}}[$fileCols[0]]\n";
 }
+print LOG "file -> table column:\t$fileCols[0]\n";
+&reorder_files( $data_table->GetAsArray( $fileCols[0] ) );
+$data_table->add_column( 'filename', @bam_files );
+$data_table->write_file("$fm->{'path'}/Samples.xls");
 
+$options->{'nameSamples'} = 'filename';
 
-$data_table->Add_2_Header( 'filename' );
+## create the LoadData.R script
 
+open( OUT, ">$fm->{'path'}/LoadData.R" )
+  or die "I could not create the LoadData.R script\n$!\n";
+print OUT "libraray(StefansExpressionSet)\n"
+  . "libraray(Rsubread)\n"
+  . "samples <- read.delim(file='Samples.xls', sep='\t', header=T)\n"
+  . "counts <- featureCounts(files =as.vector(samples[,'filename']), "
+  . join( ", ",
+	map { "$_ = '$options->{$_}'" } 'annot.ext', 'GTF.attrType',
+	'GTF.featureType',     'allowMultiOverlap',
+	'isGTFAnnotationFile', 'isPairedEnd',
+	'nthreads' )
+  . ")\n" . "\n"
+  . "$options->{'RobjName'} = NGSexpressionSet( dat = cbind(counts\$annotation,counts\$counts),"
+  . " Samples = samples,  Analysis = NULL, name='$options->{'RobjName'}', namecol='$options->{'nameSamples'}',"
+  . " namerow= '$options->{'GTF.featureType'}', usecol=NULL , outpath = NULL )\n"
+  . "save( $options->{'RobjName'}, file='$options->{'RobjName'}.RData' )\n"
+  . "save( counts, file='$options->{'RobjName'}_contsObj.RData' )\n";
+close(OUT);
+print
+"You should run the R script '$fm->{'path'}/LoadData.R' to create the data objects.\n";
 
+close(LOG);
 
+open( SCR, ">$fm->{'total'}" )
+  or die "I could not create the main script outfile $fm->{'total'}\n$!\n";
+print SCR "library(StefansExpressionSet)\n"
+  . "load('$options->{'RobjName'}.RData')\n"
+  . "## use the object from here on\n";
+close(SCR);
 
+print "Extend the R script $fm->{'total'} to work with the data\n";
 
 sub match_to_one_file {
 	my $data = shift;
-	my $r = 0;
-	foreach ( @bam_files ) {
-		$r ++ if ( $_ =~ m/$data/ );
+	my $r    = 0;
+	foreach (@bam_files) {
+		$r++ if ( $_ =~ m/$data/ );
 	}
 	return $r == 1;
 }
 
 sub check_4_link2files {
-	my ( $array ) = @_;
+	my ($array) = @_;
 	grep /\d/,
 	  map { $_ if ( &match_to_one_file( @$array[$_] ) ) }
 	  0 .. ( scalar(@$array) - 1 );
@@ -167,25 +230,28 @@ sub check_4_link2files {
 
 sub to_file_pos {
 	my $data = shift;
-	for ( my $i = 0; $i < @bam_files; $i ++ ) {
+	for ( my $i = 0 ; $i < @bam_files ; $i++ ) {
 		return $i if ( $bam_files[$i] =~ m/$data/ );
 	}
-	warn "entry '$data' did not link to a file!" ;
+	warn "entry '$data' did not link to a file!";
 	return undef;
 }
 
-
 sub reorder_files {
-	my $array = shift;
-	my @pos =  map{ &to_file_pos( $_) } @$array;
-	my @undefined = grep ( /\d+/, map { if (!defined($pos[$_])) { $_} } 0..$#pos );
+	my $array     = shift;
+	my @pos       = map { &to_file_pos($_) } @$array;
+	my @undefined = grep ( /\d+/,
+		map {
+			if ( !defined( $pos[$_] ) ) { $_ }
+		} 0 .. $#pos );
 	foreach ( sort { $b <=> $a } @undefined ) {
-        splice( @pos, $_,1 );
-        splice( @{$data_table->{'data'}}, $_, 1 ); ## drop the columns that have no file attached
+		warn "I have to drop the line $_ as no file corresponds to that id\n";
+		splice( @pos,                       $_, 1 );
+		splice( @{ $data_table->{'data'} }, $_, 1 )
+		  ;    ## drop the columns that have no file attached
 	}
-	
+	@bam_files = @bam_files[@pos];
 }
-
 
 sub is_complete {
 	my ( $self, $array ) = @_;
---
+++ t/xx_RNA_analysis_script.t
@@ -0,0 +1,65 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+use stefans_libs::root;
+use Test::More tests => 4;
+use stefans_libs::flexible_data_structures::data_table;
+
+use FindBin;
+my $plugin_path = $FindBin::Bin;
+my ( $value, $exp, @tmp, @files );
+my $exec = $plugin_path . "/../bin/RNA_analysis_script.pl";
+ok( -f $exec, "the script has been found" );
+
+if ( -d "$plugin_path/data/output/Script" ) {
+	system("rm -Rf $plugin_path/data/output/Script ");
+}
+
+foreach ( 'ERR688856.bam', 'ERR688857.bam', 'ERR688855.bam', 'ERR688858.bam' ) {
+	push ( @files, "$plugin_path/data/fake_BAM_files/$_");
+	ok( -f "$plugin_path/data/fake_BAM_files/$_", "fake bam file $_ exists" );
+}
+
+ok ( -f "$plugin_path/data/output/PRJEB7858_SUMMARY.xls", "the input summary file exists" );
+
+die "Test of script $exec not possible - please run $plugin_path/stefans_libs_XML_parser.t first\n" unless ( -f "$plugin_path/data/output/PRJEB7858_SUMMARY.xls");
+
+my $cmd =
+    "perl -I $plugin_path/../lib  $exec "
+  . " -bam_files ". join(" ", @files)
+  . " -samples $plugin_path/data/output/PRJEB7858_SUMMARY.xls"
+  . " -outfile $plugin_path/data/output/Script/PRJEB7858.R"
+  . " -options RobjName PRJEB7858 annot.ext someGTFfile.gtf GroupName 'EXPERIMENT-DESIGN-LIBRARY_DESCRIPTOR-LIBRARY_NAME' GeneName GeneID"
+  . " -debug";
+
+system($cmd );
+
+ok ( -d  "$plugin_path/data/output/Script", "Output path was created" );
+foreach ( 'PRJEB7858.R.log' , 'Samples.xls', 'PRJEB7858.R', 'LoadData.R' ) {
+	ok ( -f  "$plugin_path/data/output/Script/$_", "outfile $_ exists");
+}
+
+open ( LOG , "<$plugin_path/data/output/Script/PRJEB7858.R.log" ) or die "I could not open the log file\n$!\n";
+my @values = map { chomp;$_} <LOG>;
+close ( LOG );
+my $internal_vals;
+for ( my $i = 1; $i < @values; $i ++ ) {
+	if ( $values[$i] =~ m/^(.+)\t(.+)$/ ){
+		$internal_vals->{$1} = $2;
+	}
+}
+
+my $data_table = data_table->new( {'filename' => "$plugin_path/data/output/Script/Samples.xls"} );
+
+($internal_vals->{'filename col'}) = $data_table->Header_Position('filename');
+
+ok ( defined $internal_vals->{'file -> table column:'}, "log value 1 OK ($internal_vals->{'file -> table column:'})");
+
+for ( my $i = 0; $i < $data_table->Rows(); $i ++ ) {
+	@tmp = @{@{$data_table->{'data'}}[$i]};
+	ok ( $tmp[$internal_vals->{'filename col'}] =~ m/$tmp[$internal_vals->{'file -> table column:'}]/, "file $tmp[$internal_vals->{'filename col'}] matches corresponding accession number $tmp[$internal_vals->{'file -> table column:'}] in Samples.xls file" );
+}
+
+
+
+
